868786b7f8f01fab5d6a12ecb4903bb49f53edde
Joannis Orlandos <joannis@orlandos.nl>
2016-04-11 09:32:50 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CryptoEssentials",
     dependencies: [
-        .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 4)
     ]
 )
